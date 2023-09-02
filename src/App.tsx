80ccef5384c330a7ef141637ba77dcae676ecbc5
Ravshan Samandarov <ravshansbox@gmail.com>
2023-09-03 01:22:13 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Button } from './Button';
+
+export const App = () => {
+  return (
+    <>
+      <Button color="gray" size="xs">
+        Gray Button
+      </Button>
+      <Button color="gray" size="sm">
+        Gray Button
+      </Button>
+      <Button color="gray" size="base">
+        Gray Button
+      </Button>
+      <Button color="gray" size="md">
+        Gray Button
+      </Button>
+      <Button color="gray" size="lg">
+        Gray Button
+      </Button>
+      <Button color="gray" size="xl">
+        Gray Button
+      </Button>
+    </>
+  );
+};
